90a00134cc1b22c18c1b78d6f60d3100b1b0fc82
Rebecca Y <micelover12@gmail.com>
2023-01-07 22:55:19 +1100

    works but its spam hell

--- features/pageobjects/figment.page.js
+++ features/pageobjects/figment.page.js
@@ -30,10 +30,22 @@ class Figment extends Page {
     async exploreSpooky() {
         var exploreBtn = await $('a[class="explore-main-button"]');
         var noSteps = await $('strong');
-        console.log(await noSteps.getText());
+        console.log("steps: " + await noSteps.getText());
         exploreBtn.click();
 
-        
+        var contentElem = await $('div[class="col-xs-8 flavor"]');
+        var contentElemExist = await contentElem.isExisting();
+        var contentText;
+
+        while (contentElemExist == false) {
+            exploreBtn.click();
+            contentElem = await $('div[class="col-xs-8 flavor"]');
+            contentElemExist = await contentElem.isExisting();
+            await browser.pause(500);
+        }
+
+        contentText = await contentElem.getText();
+            console.log(contentText);
         
 
     }
